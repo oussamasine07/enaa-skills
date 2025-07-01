2bbeedfb3472670b0cb6b362627153266e1ee03c
oussamasine07 <sineoussama@gmail.com>
2025-07-01 11:47:14 +0100

    make needed models skill and subskills

---
+++ src/main/java/com/enaaskills/backend/model/SubSkill.java
@@ -0,0 +1,44 @@
+package com.enaaskills.backend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "sub_skills")
+public class SubSkill {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne
+    private Skill skill;
+
+    public SubSkill () {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Skill getSkill() {
+        return skill;
+    }
+
+    public void setSkill(Skill skill) {
+        this.skill = skill;
+    }
+}
