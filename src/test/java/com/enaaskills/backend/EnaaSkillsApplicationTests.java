261f1e9ffe2b0fef74d3481e8735b938603fba7a
oussamasine07 <sineoussama@gmail.com>
2025-07-01 11:19:01 +0100

    first commit

---
+++ src/test/java/com/enaaskills/backend/EnaaSkillsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enaaskills.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnaaSkillsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
