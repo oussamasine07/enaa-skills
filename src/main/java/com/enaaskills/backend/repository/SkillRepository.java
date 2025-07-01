f4fff0c035d351770a4b6a78f16b009bd28df5f0
oussamasine07 <sineoussama@gmail.com>
2025-07-01 12:24:35 +0100

    make skill service

--- src/main/java/com/enaaskills/backend/repository/SkillRepository.java
+++ src/main/java/com/enaaskills/backend/repository/SkillRepository.java
@@ -2,7 +2,9 @@ package com.enaaskills.backend.repository;
 
 import com.enaaskills.backend.model.Skill;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SkillRepository extends JpaRepository<Skill, Long> {
 
 }
---
+++ src/main/java/com/enaaskills/backend/service/SkillService.java
@@ -0,0 +1,58 @@
+package com.enaaskills.backend.service;
+
+import com.enaaskills.backend.model.Skill;
+import com.enaaskills.backend.repository.SkillRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class SkillService {
+
+    private final SkillRepository skillRepository;
+
+    public SkillService (
+            final SkillRepository skillRepository
+    ) {
+        this.skillRepository = skillRepository;
+    }
+
+    public ResponseEntity<?> listSkills () {
+        List<Skill> skills = skillRepository.findAll();
+
+        return new ResponseEntity<>(skills, HttpStatus.OK);
+    }
+
+    public ResponseEntity<?> getSkillById( Long skillId ) {
+        Skill skill = skillRepository.findById( skillId ).orElseThrow();
+
+        return new ResponseEntity<>(skill, HttpStatus.OK);
+    }
+
+    public ResponseEntity<?> createSkill ( Skill skill ) {
+        return new ResponseEntity<>(skillRepository.save( skill ), HttpStatus.OK);
+    }
+
+    public ResponseEntity<?> updateSkill (Skill skill, Long skillId ) {
+        Skill updatedSkill = skillRepository.findById( skillId ).orElseThrow();
+        updatedSkill.setName( skill.getName() );
+
+        return new ResponseEntity<>(skillRepository.save( updatedSkill ), HttpStatus.OK);
+    }
+
+    public ResponseEntity<?> deleteSkill ( Long skillId ) {
+        Skill deletedSkill = skillRepository.findById( skillId ).orElseThrow();
+
+        Map<String, String> success = new HashMap<>();
+        success.put("status", "success");
+        success.put("message", "skill: " + deletedSkill.getName() + " was deleted Successfully");
+
+        return new ResponseEntity<>( success, HttpStatus.OK );
+
+    }
+
+}
