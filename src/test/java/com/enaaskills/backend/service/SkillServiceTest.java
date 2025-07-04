02b1d1eee23848da0a44ba42b29d4c7a50a55542
oussamasine07 <sineoussama@gmail.com>
2025-07-04 11:45:36 +0100

    make testing for skill service

--- src/test/java/com/enaaskills/backend/service/SkillServiceTest.java
+++ src/test/java/com/enaaskills/backend/service/SkillServiceTest.java
@@ -1,25 +1,24 @@
 package com.enaaskills.backend.service;
 
 import com.enaaskills.backend.dto.mappingDTO.MappedSubSkillDTO;
-import com.enaaskills.backend.dto.mappingDTO.MappedWithSubSkillsDTO;
 import com.enaaskills.backend.mapper.SkillMapper;
-import com.enaaskills.backend.mapper.SkillMapperWithSubSkills;
 import com.enaaskills.backend.model.Skill;
 import com.enaaskills.backend.repository.SkillRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mapstruct.factory.Mappers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class SkillServiceTest {
@@ -30,14 +29,11 @@ public class SkillServiceTest {
     @Mock
     SkillMapper skillMapper;
 
-    @Mock
-    SkillMapperWithSubSkills skillMapperWithSubSkills;
-
     @InjectMocks
     private SkillService skillService;
 
     @Test
-    public void getAllSkills () {
+    void getAllSkills () {
         Skill skill1 = new Skill();
         skill1.setName("C4");
 
@@ -46,23 +42,74 @@ public class SkillServiceTest {
 
         List<Skill> skills = List.of(skill1, skill2);
 
-        // mock subSkill DTO
-        MappedSubSkillDTO sub1 = new MappedSubSkillDTO(1L, "sub 1");
-        MappedSubSkillDTO sub2 = new MappedSubSkillDTO(2L, "sub 2");
+        lenient().when(skillRepository.findAll()).thenReturn(skills);
 
-        // mock skill DTO
-        MappedWithSubSkillsDTO mappedWithSubSkillsDTO1 = new MappedWithSubSkillsDTO(1L, "skill 1", List.of(sub1));
-        MappedWithSubSkillsDTO mappedWithSubSkillsDTO2 = new MappedWithSubSkillsDTO(2L, "skill 2", List.of(sub2));
-
-        when(skillRepository.findAll()).thenReturn(skills);
-        when(skillMapperWithSubSkills.toDTO(skill1)).thenReturn(mappedWithSubSkillsDTO1);
-        when(skillMapperWithSubSkills.toDTO(skill2)).thenReturn(mappedWithSubSkillsDTO2);
-
-        ResponseEntity<?> result = skillService.listSkills();
+        ResponseEntity result = skillService.listSkills();
 
         assertEquals(HttpStatus.OK, result.getStatusCode());
 
+    }
 
+    @Test
+    void getSkillById () {
+        Skill skill = new Skill();
+        skill.setId(1L);
+        skill.setName("C4");
+
+        lenient().when(skillRepository.findById(1L)).thenReturn(Optional.of(skill));
+
+        ResponseEntity result = skillService.getSkillById(1L);
+
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+
+    }
+
+    @Test
+    void createSkill () {
+        Skill skill = new Skill();
+        skill.setId(1L);
+        skill.setName("C4");
+
+        when(skillRepository.save(any(Skill.class))).thenReturn(skill);
+
+        ResponseEntity<Skill> result = (ResponseEntity<Skill>) skillService.createSkill( skill );
+
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+        assertEquals("C4", result.getBody().getName());
+
+    }
+
+    @Test
+    void updateSkill () {
+        Skill skill = new Skill();
+        skill.setId(1L);
+        skill.setName("C4");
+
+        Skill updatedSkill = new Skill();
+        updatedSkill.setId(1L);
+        updatedSkill.setName("new skill");
+
+        lenient().when(skillRepository.findById(1L)).thenReturn(Optional.of(skill));
+        when(skillRepository.save(any(Skill.class))).thenAnswer(invocationOnMock -> invocationOnMock.getArgument(0));
+
+        ResponseEntity<Skill> result = (ResponseEntity<Skill>) skillService.updateSkill(updatedSkill,1L);
+
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+        assertEquals("new skill", result.getBody().getName());
+    }
+
+    @Test
+    void deleteSkill () {
+        Skill skill = new Skill();
+        skill.setId(1L);
+        skill.setName("C4");
+        lenient().when(skillRepository.findById(1L)).thenReturn(Optional.of(skill));
+        doNothing().when(skillRepository).deleteById(1L);
+
+        ResponseEntity<Map<String, String>> result = (ResponseEntity<Map<String, String>>) skillService.deleteSkill(1L);
+
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+        assertEquals("success", result.getBody().get("status"));
 
     }
 
